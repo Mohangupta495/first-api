a29bf9fb7e6f604f915e5ffda6feeaf52bbb25ca
Mohan Gupta <75502789+Mohangupta495@users.noreply.github.com>
2023-02-26 15:24:18 +0530

    add new data

--- data.js
+++ data.js
@@ -96,3 +96,5 @@ const products = [
     image: "",
   },
 ];
+
+module.exports = products;
--- index.js
+++ index.js
@@ -9,13 +9,29 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hey this is my APdjffjdlj nowfdjfjdlsfjdsfljI running 🥳");
+  res.send("welocme to webbrings");
 });
 
 app.get("/about", (req, res) => {
   res.send("This is my about route..... ");
 });
-app.get("products", (req, res) => {
+app.post("/sendmail", (req, res) => {
+  const data = new Model({
+    name: req.body.name,
+    email: req.body.email,
+    desc: req.body.desc,
+    phone: req.body.phone,
+  });
+
+  try {
+    const dataToSave = data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+  // res.send("This is my about route..... ");
+});
+app.get("/products", (req, res) => {
   res.send(products);
 });
 
